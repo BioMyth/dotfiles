53136ae3f2fdd9259d2f039c5f7e740b17a2175a
BioMyth <biomyth1@gmail.com>
2025-02-27 21:35:44 -0500

    Added a complete install script, added some ssh bs, & a small software summary

--- Readme.md
+++ Readme.md
@@ -10,4 +10,24 @@ This is a basic repository holding all of my modified dotfiles for ease and port
 
 ## Usage
 
+## Tools
+
+This is a list of commond tools that are needed/used by this dotfiles repo & I want installed
+
+* dua-cli [dua i] - Fast effective disk usage utility
+    * ncdu [ncdu] - Similar functional tool but is slower
+* bottom [btm] - Great system monitoring tool, best performance
+    * htop [htop] - Similar as above, worse performance
+    * btop [btop] - Really good looking system monitoring tool
+* ctop [ctop] - Cool container usage monitoring tool
+* neovim [nvim] - Greate vim replacement, chosen over vim for the more modern codebase & maintainers
+    * vim [vim] - Still a great tool
+* foot [foot] - Current favorite terminal emulator, no gpu acceleration but the performance is truly amazing & lightweight
+    * wezterm - Really good terminal emulator, too heavy & bulky IMO
+    * ghostty - Similar to wezterm
+    * alacritty - Great terminal emulatory, gpu accelerated, but not seeming as good as foot for my uses
+    * kitty - Similar to alacritty, dislike due to large python codebase & not following standards as much (e.g. kitty image display)
+* tmux [tmux] - Terminal multiplexer
+    * screen [screen] - Similar to tmux, arbitrarily chose tmux above as standard
+    * zellij [zellij] - terminal multiplexer with discoverable ui & webassembly plugins, like the design but isn't compact enough & don't love the performance/ideology
 
---
+++ install.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/bash
+
+dry_run=false
+
+
+while getopts dh: opt; do
+    case $opt in
+        d) dry_run=true ;;
+        h) echo "This command installs the provided dotfiles excluding the manual install scripts. Provide -d for a dry run."
+            exit 0 ;;
+        *) echo "Error in command line parsing parameter provided opt is not an allowed operation" >&2
+            exit 1
+    esac
+done
+
+config_path=${XDG_CONFIG_HOME:-$HOME/.config}
+
+dotfile_path=$(realpath "$0" | sed 's|\(.*\)/.*|\1|')
+
+
+if [ "$dry_run" = true ]; then
+    echo "Running a dry run"
+    echo "================================"
+fi
+
+
+function link {
+    local source=$1
+    local dest=$2
+    # If destination exists & is a symbolic or hard link
+    if [ -e "$dest" ] && ( [ -L "$dest" ] || [ $(stat -c '%h' "$dest") > 1 ] ); then
+        echo "Skipping $dest already is a symlink";
+        return 0;
+    else
+        echo "$dest    ->  $source";
+    fi
+    
+    if [ "$dry_run" = true ]; then
+        :
+       # Soft Link Directories
+    elif [ -d "$source" ]; then
+        ln -sb "$source" "$dest";
+    else
+        ln -b "$source" "$dest";
+    fi
+}
+
+function install {
+    local dest_dir="$1"
+    shift
+    local sources="$@"
+    for source in "$@"; do
+        local base_name=$(basename -a $source)
+        link "$source" "$dest_dir$base_name"
+    done
+}
+
+
+# Find all top level dotfiles to install
+readarray -d '' install_files < <(find "$dotfile_path/" -maxdepth 2 -mindepth 2 \( -path "$dotfile_path/.git*" -o -path "$dotfile_path/manual-install*"  -o -name '.*' \) -o -prune -type f -print0 )
+
+# Install the dotfiles
+install "$HOME/." "${install_files[@]}"
+
+# Find all the directories in config subfolders, they should map to XDG_CONFIG_DIR
+readarray -d '' install_folders < <(find /home/casteels/dotfiles/ -maxdepth 3 -mindepth 3 -path "$dotfile_path/*/config/*" -type d -print0)
+
+install "$config_path/" "${install_folders[@]}"
+
